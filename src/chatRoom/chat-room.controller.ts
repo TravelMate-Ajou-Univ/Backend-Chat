83fa7f83dd7bbb90cad9586fad5ebe4ae851bd34
YuuuJeong <dbwjdgh03@ajou.ac.kr>
2023-11-26 20:50:22 +0900

    @CurrentUser 데코레이터 주석해제

--- src/chatRoom/chat-room.controller.ts
+++ src/chatRoom/chat-room.controller.ts
@@ -13,15 +13,14 @@ export class ChatRoomController {
 
   @ApiOperation({ summary: '채팅방 생성' })
   @ApiResponse({})
-  // @ApiBearerAuth()
-  // @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @Post('chatroom')
   async createChatRoom(
     @CurrentUser() user: UserEntity,
     @Body() dto: CreateChatRoomDto,
   ): Promise<CreateChatRoomResponseDto> {
-    const userId = 1;
-    return await this.chatRoomService.createChatRoom(dto, userId);
+    return await this.chatRoomService.createChatRoom(dto, user.id);
   }
 
   @ApiOperation({ summary: '나의 채팅방 조회' })
